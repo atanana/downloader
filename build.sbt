e99521be76a4dec41aad73c9bd59fa63e718f8ac
Andrei Tanana <atanana@ampsoft.by>
2016-07-13 16:25:19 +0300

    init

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "downloader"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "net.liftweb" % "lift-json_2.11" % "3.0-RC2"
+libraryDependencies += "org.scalaj" % "scalaj-http_2.11" % "2.3.0"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,38 @@
+import java.nio.file.{Files, Paths}
+import java.util.concurrent.Executors
+
+import net.liftweb.json._
+
+import scala.concurrent._
+import scalaj.http._
+
+object Main {
+  def request(link: String): HttpResponse[String] = {
+    Http(link).asString
+  }
+
+  def filename(link: String): String = {
+    val index: Int = link.lastIndexOf('/')
+    link.substring(index + 1)
+  }
+
+  def cut(link: String): String = link.substring(0, link.lastIndexOf('/'))
+
+  def main(args: Array[String]) {
+    val json = request(args.head).body
+    val linksNodes: JsonAST.JValue = parse(json) \ "threads" \\ "posts" \\ "path"
+    val relativePath: String = cut(cut(args.head))
+    val links: List[String] = linksNodes.children.map(relativePath + "/" + _.values.toString)
+    val downloadFolder = Paths.get(System.getProperty("user.dir"), "downloads_" + System.currentTimeMillis())
+    downloadFolder.toFile.mkdirs()
+    implicit val executor: ExecutionContextExecutor = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
+
+    Await.ready(Future.sequence(links.map(link => Future {
+      val file: String = filename(link)
+      Files.write(Paths.get(downloadFolder.toString, file), Http(link).asBytes.body)
+      println(file)
+    })), duration.Duration.Inf)
+
+    System.exit(0)
+  }
+}
